a8f65bbafb15940333453d4610c8877f34e1cf9d
Christel Mogollon <christel.floresm@gmail.com>
2019-01-10 13:11:49 -0500

    installed sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-  before_action :set_raven_context
 
-  def set_raven_context
-    Raven.user_context(id: session[:user_id]) # or anything else in session
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
 end
