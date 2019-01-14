8f0ceeb0015120b9fdc5c8a2e2b26a919fe65c96
Christel Mogollon <christel.floresm@gmail.com>
2019-01-14 10:07:57 -0500

    fixed Sentry env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    # config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 
 
